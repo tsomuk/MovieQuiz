99b8d6f3b1689cc08c9914fa4ffbd8b2526d0d5e
tsomuk <tsomuk@me.com>
2024-01-30 22:14:18 +0200

    Initial Commit

---
+++ MovieQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieQuiz
+//
+//  Created by Nikita Tsomuk on 30/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
